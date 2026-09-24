63e7cbf531e8a90a65524a4de16f874ca50a139c
nathanmizael-pixel <nathanmizael@gmail.com>
2026-09-24 12:12:39 -0300

    fix: update Discord invite link to aHWeZpZYvK

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
   twitch: 'https://twitch.tv/nicotinacat',
   tiktok: 'https://tiktok.com/@nicotinaclipes',
-  discord: 'https://discord.gg/nicotinacat',
+  discord: 'https://discord.gg/aHWeZpZYvK',
   amazon: 'https://www.amazon.com.br/hz/wishlist/ls/1EKY0Z6C63WIS?ref_=wl_share',
 };
